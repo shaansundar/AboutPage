32a8518be5ca4d30e1a6064edea352c692576d9f
Shaan Sundar <shaan.idtindia@gmail.com>
2021-07-09 12:22:02 +0530

    Styled button, added about_us

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
 
-
+// #eff4f7
 const Navbar = () =>{
     return (
      <div className="head">
    <header class="text-gray-600 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-    <Image  src="/logo.png" width={128} height={77} />
+    <Image  src="/logo.png" width={187.5} height={87.45} />
      </a>
     <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
     <Link href="/"><a class="mr-5 hover:text-gray-900"><span className="txt">Home</span></a></Link>
     <Link href="/Nft"><a class="mr-5 hover:text-gray-900"><span className="txt">CrazyNFT</span></a></Link>
     <Link href="/blog"><a class="mr-5 hover:text-gray-900"><span className="txt">NFT BLOG</span></a></Link>  
    </nav>
-    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-    <span className="txt"> LAUNCH APP </span>
-      
+    <button class="inline-flex items-center text-white bg-gradient-to-r from-green-dark to-green-light hover:bg-gray-200 border-0 w-buttonwidth h-buttonheight py-1 px-3 focus:outline-none rounded mt-4 md:mt-0">
+    <span className="txtw"> LAUNCH APP </span> 
     </button>
   </div>
   </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,32 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      green: {
+        light:'#00C0B6',
+        dark:'#015050',
+
+      },
+      pink: {
+        light: '#ff7ce5',
+        DEFAULT: '#ff49db',
+        dark: '#ff16d1',
+      },
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#c0ccda',
+        light: '#e0e6ed',
+        lightest: '#f9fafc',
+      }
+    },
+    extend: {
+      spacing: {
+        'buttonwidth': '180px',
+        'buttonheight': '60px',
+      }
+     }
+  }
 }
