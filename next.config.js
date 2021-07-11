caba372631350a7bfc965f572fa17ac9c0ea5f9b
Shaan Sundar <shaan.idtindia@gmail.com>
2021-07-11 21:39:35 +0530

    errors fixed

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   reactStrictMode: true,
+  future: {
+    webpack5: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      // issuer: {
+      //   test: /\.(js|ts)x?$/,
+      // },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              },
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
 }
