f6436ce4b759d8c1a569dce1d830d626d0d214a8
Shaan Sundar <shaan.idtindia@gmail.com>
2021-07-28 12:12:18 +0530

    Added input and roadmap

--- components/Hero.js
+++ components/Hero.js
@@ -43,7 +43,13 @@ function Hero(props) {
             <span className="txtw">Launch App</span>
           </button> 
           </Link>*/}
-          <button class=" 
+          <div class="flex w-full md:justify-start justify-center items-end">
+        <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
+          <label for="hero-field" class="leading-7 text-sm text-gray-600">Placeholder</label>
+          <input type="text" id="hero-field" name="hero-field" class="w-full bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-indigo-200 focus:bg-transparent text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+        </div>
+        {/* <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button> */}
+        <button class="
                     inline-flex 
                     items-center  
                     bg-gray-headerbg
@@ -62,6 +68,8 @@ function Hero(props) {
                     >
             <span className="secondarybuttontxt">Read Whitepaper</span>
           </button>
+      </div>
+          
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Hero from "../components/Hero"
 import LightFeatureB from "../components/Feature";
+import RoadMap from "../components/RoadMap";
 import styles from "../styles/Home.module.css";
 import Banner from "../components/Banner";
 import { motion } from 'framer-motion';
@@ -11,8 +12,8 @@ export default function Home() {
     <div>
       <Hero />
       <LightFeatureB />
+      <RoadMap />
       <Banner />
-      
     </div>
   );
 }
